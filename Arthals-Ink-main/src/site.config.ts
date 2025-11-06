5cbfa34b5514d2d41544f2b0ca36fef0410c7f65
typolab <oicsci@gmail.com>
2025-11-06 23:47:21 +0800

    first

---
+++ Arthals-Ink-main/src/site.config.ts
@@ -0,0 +1,146 @@
+import type { Config, IntegrationUserConfig, ThemeUserConfig } from 'astro-pure/types';
+
+
+
+
+
+export const theme = {
+  // === Basic configuration ===
+  /** Title for your website. Will be used in metadata and as browser tab title. */
+  title: "CiZai' ink",
+  /** Will be used in index page & copyright declaration */
+  author: 'CiZai',
+  since: 2025,
+  /** Description metadata for your website. Can be used in page metadata. */
+  description: '栀子比众木  人间诚未多',
+  /** The default favicon for your site which should be a path to an image in the `public/` directory. */
+  favicon: '/favicon/favicon.ico',
+  /** Specify the default language for this site. */
+  locale: {
+    lang: 'en-US',
+    attrs: 'en_US',
+    // Date locale
+    dateLocale: 'en-US',
+    dateOptions: {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    }
+  },
+  /** Set a logo image to show in the homepage. */
+  logo: {
+    src: 'src/assets/avatar.png',
+    alt: 'Avatar'
+  },
+
+  // === Global configuration ===
+  titleDelimiter: '•',
+  prerender: true,
+  npmCDN: 'https://cdn.jsdelivr.net/npm',
+
+  // Still in test
+  head: [
+    /* Telegram channel */
+    // {
+    //   tag: 'meta',
+    //   attrs: { name: 'telegram:channel', content: '@cworld0_cn' },
+    //   content: ''
+    // }
+  ],
+  customCss: [],
+
+  /** Configure the header of your site. */
+  header: {
+    menu: [
+      { title: 'Blog', link: '/blog' },
+      { title: 'Articles', link: '/articles' },
+      { title: 'Archive', link: '/projects' },
+      { title: 'About', link: '/about' }
+    ]
+  },
+
+  /** Configure the footer of your site. */
+  footer: {
+    // Year format
+    year: `© ${new Date().getFullYear()}`,
+    // year: `© 2019 - ${new Date().getFullYear()}`,
+    links: [
+      // Registration link
+
+    ],
+    /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
+    credits: false,
+    /** Optional details about the social media accounts for this site. */
+    social: { github: 'https://github.com/CiZaii', email: 'mailto:zang@linux.do' }
+  },
+
+  content: {
+    /** External links configuration */
+    externalLinks: {
+      content: ' ↗',
+      /** Properties for the external links element */
+      properties: {
+        style: 'user-select:none'
+      }
+    },
+    /** Blog page size for pagination (optional) */
+    blogPageSize: 8,
+    // Currently support weibo, x, bluesky
+    share: ['weibo']
+  }
+}
+
+export const integ: IntegrationUserConfig = {
+  // Enable page search function
+  pagefind: true,
+  // Add a random quote to the footer (default on homepage footer)
+  // See: https://astro-pure.js.org/docs/integrations/advanced#web-content-render
+  quote: {
+    // https://developer.hitokoto.cn/sentence/#%E8%AF%B7%E6%B1%82%E5%9C%B0%E5%9D%80
+    server: 'https://v1.hitokoto.cn/?c=i',
+    target: `(data) => data.hitokoto || 'Error'`
+    // https://github.com/lukePeavey/quotable
+    // server: 'https://api.quotable.io/quotes/random?maxLength=60',
+    // target: `(data) => data[0].content || 'Error'`
+  },
+  // UnoCSS typography
+  // See: https://unocss.dev/presets/typography
+  typography: {
+    class: 'prose text-base text-muted-foreground',
+    // The style of blockquote font, normal or italic (default to italic in typography)
+    blockquoteStyle: 'italic',
+    // The style of inline code block, code or modern (default to code in typography)
+    inlineCodeBlockStyle: 'modern'
+  },
+  // A lightbox library that can add zoom effect
+  // See: https://astro-pure.js.org/docs/integrations/others#medium-zoom
+  mediumZoom: {
+    enable: true, // disable it will not load the whole library
+    selector: '.prose .zoomable',
+    options: {
+      className: 'zoomable'
+    }
+  },
+  // Comment system
+  waline: {
+    enable: true,
+    // Server service link
+    server: 'https://commit.wl.do',
+    // Refer https://waline.js.org/en/guide/features/emoji.html
+    emoji: ['bmoji', 'weibo'],
+    // Refer https://waline.js.org/en/reference/client/props.html
+    additionalConfigs: {
+      // search: false,
+      pageview: true,
+      comment: true,
+      locale: {
+        reaction0: 'Like',
+        placeholder: 'Welcome to comment. (Email to receive replies. Login is unnecessary)'
+      },
+      imageUploader: false
+    }
+  }
+}
+
+const config = { ...theme, integ } as Config
+export default config
